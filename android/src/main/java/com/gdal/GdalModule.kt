d3a5c8ef83068a3a568b7c59c59e7323fa5c9c72
Ta Minh Huy <huytm2k3@gmail.com>
2024-10-08 15:37:55 +0700

    feat: Android support GdalInfo, OgrInfo

--- android/src/main/java/com/gdal/GdalModule.kt
+++ android/src/main/java/com/gdal/GdalModule.kt
@@ -1,10 +1,25 @@
 package com.gdal
 
+import android.util.Log
+import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableArray
+import com.facebook.react.bridge.WritableArray
+import org.gdal.gdal.Dataset
+import org.gdal.gdal.InfoOptions
+import org.gdal.gdal.VectorInfoOptions
+import org.gdal.gdal.gdal
+import org.gdal.gdal.gdal.AllRegister
+import org.gdal.gdal.gdal.GDALInfo
+import org.gdal.ogr.ogr
+import org.gdal.gdal.gdal.GDALVectorInfo
+import org.gdal.gdal.gdal.Open
+import org.gdal.gdal.gdal.GetLastErrorMsg
+import org.gdal.gdal.gdal.OpenEx
+import java.util.Vector
 
 
 class GdalModule(reactContext: ReactApplicationContext) :
@@ -15,9 +30,33 @@ class GdalModule(reactContext: ReactApplicationContext) :
   }
 
   @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
+  fun getDrivers(promise: Promise) {
+    try {
+      // Register OGR drivers
+      AllRegister();
+
+      // Create a list to store driver names
+      val drivers = ArrayList<String>()
+      for (i in 0 until gdal.GetDriverCount()) {
+        val driver = gdal.GetDriver(i)
+        drivers.add(driver.shortName)
+      }
+
+      // Convert the list of driver names to a WritableArray
+      val driversArray: WritableArray = Arguments.createArray()
+      for (driver in drivers) {
+        driversArray.pushString(driver)
+      }
+
+      // Resolve the promise with the WritableArray
+      promise.resolve(driversArray)
+    } catch (e: Exception) {
+      // In case of error, reject the promise
+      promise.reject("ERROR_GETTING_DRIVERS", e)
+    }
   }
+
+
 
   @ReactMethod
   fun RNOgr2ogr(args: ReadableArray, promise: Promise) {
@@ -33,26 +72,42 @@ class GdalModule(reactContext: ReactApplicationContext) :
 
   @ReactMethod
   fun RNOgrinfo(args: ReadableArray, promise: Promise) {
-    // Convert ReadableArray to String[]
+    AllRegister()
     val strArgs = arrayOfNulls<String>(args.size())
     for (i in 0 until args.size()) {
       strArgs[i] = args.getString(i)
     }
-    // Pass the string array to ogrinfo
-    ogrinfo.main(strArgs)
-    promise.resolve("Success")
+    val inputString = args.getString(0)
+    val args = Vector<String>()
+    for (i in strArgs.indices) {
+      if (i != 0) {
+        args.add(strArgs[i])
+      }
+    }
+    val dataset = OpenEx(inputString)
+    var vectorInfoOptions = VectorInfoOptions(args)
+    val ogrInfo = GDALVectorInfo(dataset, vectorInfoOptions)
+    promise.resolve(ogrInfo)
   }
 
   @ReactMethod
   fun RNGdalinfo(args: ReadableArray, promise: Promise) {
-    // Convert ReadableArray to String[]
+    AllRegister()
     val strArgs = arrayOfNulls<String>(args.size())
     for (i in 0 until args.size()) {
       strArgs[i] = args.getString(i)
     }
-    // Pass the string array to gdalinfo
-    gdalinfo.main(strArgs)
-    promise.resolve("Success")
+    val inputString = args.getString(0)
+    val args = Vector<String>()
+    for (i in strArgs.indices) {
+      if (i != 0) {
+        args.add(strArgs[i])
+      }
+    }
+    val dataset = OpenEx(inputString)
+    var infoOptions = InfoOptions(args)
+    val gdalInfo = GDALInfo(dataset, infoOptions)
+    promise.resolve(gdalInfo)
   }
 
   @ReactMethod
